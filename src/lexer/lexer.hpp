5c3bd3ee077756472ce29a8e232303afc664edc7
David Moreno Jimenez <davmoren@c3r1s1.42urduliz.com>
2026-02-11 04:55:11 +0100

    update

--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -8,137 +8,172 @@
 
 class TokenType
 {
-    private:
-        std::unordered_map<std::string, TokenType> map;
+private:
+    
+    
+
+public:
+    TokenType() {};
+    ~TokenType() {};
+    struct ExecutionContext
+    {
+        
+        std::unordered_map<std::string, int> variables;
+        std::unordered_map<std::string, int> functions;
+        std::unordered_map<std::string, int> classes;
+        std::unordered_map<std::string, int> whileLoops;
+        std::unordered_map<std::string, int> forLoops;
+        std::unordered_map<std::string, int> ifStatements;
+        std::unordered_map<std::string, int> elseIfStatements;
+        std::unordered_map<std::string, int> elseStatements;
+        ExecutionContext *next;
+    };
+    struct NodeContent
+    {
         std::string value;
         std::string type;
-        int tokenTypeInt; 
-        std::unordered_map<std::string, TokenType> punctuation;
-        std::unordered_map<std::string, TokenType> operators ;
-        std::unordered_map<std::string, TokenType> keywords ;
+        int tokenTypeInt;
+        std::string punctuation;
+        std::string operators;
+        std::string keywords;
+        ExecutionContext *context;
+    };
+    
+    enum class PrincipalType
+    {
+        Variable,
+        Function,
+        Class,
+        WhileLoop,
+        ForLoop,
+        IfStatement,
+        ElseIfStatement,
+        ElseStatement,
+        Export,
+        Include,
+        Define,
+    };
+    enum class NodeType
+    {
 
-    public:
-            TokenType() {};
-            ~TokenType() {};
+        Program,
+        Statement,
+        Expression,
+        Term,
+        Factor
+    };
+    enum class Type
+    {
+        Identifier = 0,
+        Keyword = 1,
+        Operator = 2,
+        Punctuation = 3,
+        Number = 4,
+        String = 5,
+        EndOfFile = 6
+    };
+    enum class OperatorType
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide
+    };
+    enum class ValueType
+    {
+        Number,
+        Identifier,
+        String
+    };
 
-        struct ExecutionContext
-        {
-            std::unordered_map<std::string, int> variables;
-            std::unordered_map<std::string, int> functions;
-            std::unordered_map<std::string, int> classes;
-            std::unordered_map<std::string, int> whileLoops;
-            std::unordered_map<std::string, int> forLoops;
-            std::unordered_map<std::string, int> ifStatements;
-            std::unordered_map<std::string, int> elseIfStatements;
-            std::unordered_map<std::string, int> elseStatements;
-        };
-        enum class NodeType
-        {
-            Program,
-            Statement,
-            Expression,
-            Term,
-            Factor
-        };
-        enum  class Type
-        {
-            Identifier= 0,
-            Keyword= 1,
-            Operator= 2,
-            Punctuation= 3,
-            Number= 4,
-            String= 5,
-            EndOfFile= 6
-        };
-        enum class OperatorType
-        {
-            Plus,
-            Minus,
-            Multiply,
-            Divide
-        };
-        enum class ValueType
-        {
-            Number,
-            Identifier,
-            String
-        };
-        
-        std::unordered_map<std::string, TokenType::Type> punctuation = {
-            {";", TokenType::Type::Punctuation},
-            {",", TokenType::Type::Punctuation},
-            {"(", TokenType::Type::Punctuation},
-            {")", TokenType::Type::Punctuation},
-            {"{", TokenType::Type::Punctuation},
-            {"}", TokenType::Type::Punctuation},
-        } ;
-        std::unordered_map<std::string, TokenType::Type> operators = {
-            {"+", TokenType::Type::Operator},
-            {"-", TokenType::Type::Operator},
-            {"*", TokenType::Type::Operator},
-            {"/", TokenType::Type::Operator},
-            {"=", TokenType::Type::Operator}};
-        std::unordered_map<std::string, TokenType::Type> keywords ={
-            {"if", TokenType::Type::Keyword},
-            {"else", TokenType::Type::Keyword},
-            {"while", TokenType::Type::Keyword},
-            {"for", TokenType::Type::Keyword},
-            {"return", TokenType::Type::Keyword}}; 
-        void parseStatement(Token token);
-        NodeType getNodeType() const { return NodeType::Statement; }
-        std::unordered_map<std::string, TokenType> getPunctuation() const { return punctuation; }
-        std::unordered_map<std::string, TokenType> getOperators() const { return operators; }
-        std::unordered_map<std::string, TokenType> getKeywords() const { return keywords; }
-        std::string getValue() const { return value; }
-        std::string getType() const { return type; }
-        int getTokenTypeInt() const { return tokenTypeInt; }
-        std::unordered_map<std::string, TokenType> load_token_map(TokenType::Type type) const;
-        std::unordered_map<std::string, TokenType> setTokenMap(const std::unordered_map<std::string, TokenType> &new_map);
-        std::unordered_map<std::string, TokenType>
+    std::unordered_map<std::string, TokenType::Type> punctuation = {
+        {";", TokenType::Type::Punctuation},
+        {",", TokenType::Type::Punctuation},
+        {"(", TokenType::Type::Punctuation},
+        {")", TokenType::Type::Punctuation},
+        {"{", TokenType::Type::Punctuation},
+        {"}", TokenType::Type::Punctuation},
+    };
+    std::unordered_map<std::string, TokenType::Type> operators = {
+        {"+", TokenType::Type::Operator},
+        {"-", TokenType::Type::Operator},
+        {"*", TokenType::Type::Operator},
+        {"/", TokenType::Type::Operator},
+        {"=", TokenType::Type::Operator}};
+    std::unordered_map<std::string, TokenType::Type> keywords = {
+        {"if", TokenType::Type::Keyword},
+        {"else", TokenType::Type::Keyword},
+        {"while", TokenType::Type::Keyword},
+        {"for", TokenType::Type::Keyword},
+        {"return", TokenType::Type::Keyword}};
+    std::unordered_map<std::string, TokenType::PrincipalType> priority = {
+        {"var", TokenType::PrincipalType::Variable},
+        {"function", TokenType::PrincipalType::Function},
+        {"class", TokenType::PrincipalType::Class},
+        {"#include", TokenType::PrincipalType::Include},
+        {"#define", TokenType::PrincipalType::Define},
+        {"export", TokenType::PrincipalType::Export}};
+    void parseStatement(Token token);
+    NodeType getNodeType() const;
+    NodeContent *createNode(const std::string &value, const std::string &type);
+    ExecutionContext createExecutionContext() const;
+    std::unordered_map<std::string, TokenType> readExecutionContext(const ExecutionContext &context);
+    int orderExecutionContext(const ExecutionContext &context);
+    std::unordered_map<std::string, TokenType::Type> load_token_map(TokenType::Type type, std::string value);
+    std::unordered_map<std::string, TokenType> setTokenMap(std::string value, TokenType type,std::unordered_map<std::string, TokenType> token_map);
+    std::unordered_map<std::string, TokenType> setPuntuation(const std::unordered_map<std::string, TokenType> &new_map, int tokenTypeInt );
+    std::unordered_map<std::string, TokenType> setOperators(const std::unordered_map<std::string, TokenType> &new_map);
+    std::unordered_map<std::string, TokenType> setKeywords(const std::unordered_map<std::string, TokenType> &new_map);
+    std::string setvalue(std::string value);
+    ExecutionContext createExecutionContext() const;
+    std::unordered_map<std::string, TokenType> readExecutionContext(const ExecutionContext &context);
+    std::unordered_map<std::string, TokenType::NodeContent > TokenType::setTokenPriorityMap(std::string value, TokenType::PrincipalType principaltype , TokenType type,TokenType::NodeContent content, int i);
+    std::string getNodeTypeString(NodeType nodeType);
+    std::string getPrincipalTypeString(PrincipalType principalType);
+    std::string getTypeString(Type type);
 };
 
 class Token
 {
-    private:
-        TokenType type;
-        std::string value;
-        int tokenTypeInt;
-        std::unordered_map<std::string, TokenType> map;
+private:
+    TokenType type;
+    std::string value;
+    int tokenTypeInt;
+    std::unordered_map<std::string, int, TokenType> map;
 
-    public:
-        Token(TokenType type, const std::string &value) : type(type), value(value) {}
-        ~Token() {}
-        TokenType getType() const { return type; }
-        std::string getValue() const { return value; }
-        TokenType load_token_map(TokenType tipe) const;
-        std::unordered_map<std::string, TokenType> token_map(TokenType type, const std::string &value);
-        std::unordered_map<std::string, TokenType> getTokenMap() const { return map; }
-        std::unordered_map<std::string, TokenType> setTokenMap(const std::unordered_map<std::string, TokenType> &new_map);
-        std::unordered_map<std::string, TokenType> load_token_map(std::unordered_map<std::string, TokenType> tipe);
-        std::unordered_map<std::string, TokenType> setvalue(std::unordered_map<std::string, TokenType> new_map);
-        int getTokenTypeInt(TokenType type);
-        
+public:
+    Token(TokenType type, const std::string &value) : type(type), value(value) {}
+    ~Token() {}
+    TokenType getType() const { return type; }
+    std::string getValue() const { return value; }
+    TokenType load_token_map(TokenType tipe) const;
+    std::unordered_map<std::string, TokenType> token_map(TokenType type, const std::string &value);
+    std::unordered_map<std::string, int, TokenType> getTokenMap() const { return map; }
+    std::unordered_map<std::string, TokenType> setTokenMap(const std::unordered_map<std::string, TokenType> &new_map);
+    std::unordered_map<std::string, TokenType> load_token_map(std::unordered_map<std::string, TokenType> tipe);
+    std::unordered_map<std::string, TokenType> setvalue(std::unordered_map<std::string, TokenType> new_map);
+    int getTokenTypeInt(TokenType type);
 };
 
 class Lexer
 {
-    private:
-        std::string input;
-        size_t position;
-        size_t read_position;
-        char current_char;
-        bool is_end_of_file;
-        bool is_loop;
+private:
+    std::string input;
+    size_t position;
+    size_t read_position;
+    char current_char;
+    bool is_end_of_file;
+    bool is_loop;
 
-    public:
-        Lexer(const std::string &input) : input(input), position(0), read_position(0), current_char(0), is_end_of_file(false), is_loop(false) {};
-        void readChar();
-        Token nextToken();
-        void skipWhitespace();
-        Token readIdentifier();
-        Token readNumber();
-        Token readString();
-        bool isLetter(char ch) const;
+public:
+    Lexer(const std::string &input) : input(input), position(0), read_position(0), current_char(0), is_end_of_file(false), is_loop(false) {};
+    void readChar();
+    Token nextToken();
+    void skipWhitespace();
+    Token readIdentifier();
+    Token readNumber();
+    Token readString();
+    bool isLetter(char ch) const;
 };
 
 #endif // LEXER_HPP
--- src/lexer/token.cpp
+++ src/lexer/token.cpp
@@ -7,20 +7,94 @@ std::unordered_map<std::string, TokenType> Token::token_map(TokenType type, cons
     return token_map;
 }
 
-std::unordered_map<std::string, TokenType> Token::load_token_map(std::unordered_map<std::string, TokenType> tipe)
+std::string TokenType::getNodeTypeString(NodeType nodeType)
 {
-    std::string example_value = "if";
-    setvalue(Token::token_map(TokenType::Identifier, "if"));
-    return tipe;
+    switch (nodeType)
+    {
+    case NodeType::Program:
+        return "Program";
+    case NodeType::Statement:
+        return "Statement";
+    case NodeType::Expression:
+        return "Expression";
+    case NodeType::Term:
+        return "Term";
+    case NodeType::Factor:
+        return "Factor";
+    default:
+        return "Unknown";
+    }
+}
+std::string TokenType::getPrincipalTypeString(PrincipalType principalType)
+{
+    switch (principalType)
+    {
+    case PrincipalType::Variable:
+        return "Variable";
+    case PrincipalType::Function:
+        return "Function";
+    case PrincipalType::Class:
+        return "Class";
+    case PrincipalType::WhileLoop:
+        return "WhileLoop";
+    case PrincipalType::ForLoop:
+        return "ForLoop";
+    case PrincipalType::IfStatement:
+        return "IfStatement";
+    case PrincipalType::ElseIfStatement:
+        return "ElseIfStatement";
+    case PrincipalType::ElseStatement:
+        return "ElseStatement";
+    case PrincipalType::Export:
+        return "Export";
+    case PrincipalType::Include:
+        return "Include";
+    case PrincipalType::Define:
+        return "Define";
+    default:
+        return "Unknown";
+    }
 }
 
-std::unordered_map<std::string, TokenType> Token::setvalue(std::unordered_map<std::string, TokenType> new_map)
+std::unordered_map<std::string, TokenType::NodeContent> TokenType::setTokenPriorityMap(std::string value, TokenType::PrincipalType principaltype , TokenType type,TokenType::NodeContent content, int i )
 {
-    map = new_map;
-    return map;
+    std::unordered_map<std::string, TokenType::NodeContent> token_map;
+    if(getPrincipalTypeString(principaltype) == "var" || getPrincipalTypeString(principaltype) == "function" || getPrincipalTypeString(principaltype) == "class" || getPrincipalTypeString(principaltype) == "#include" || getPrincipalTypeString(principaltype) == "#define" || getPrincipalTypeString(principaltype) == "export")
+    {
+        content.type.replace(0, content.type.size(), "Identifier");
+        ExecutionContext context;
+        content.context = &context;
+        context.variables.insert({ value, i});
+        token_map.insert({value, content});
+    }
+    
+    return token_map;
 }
 
-int Token::getTokenTypeInt(TokenType type)
+
+
+std::unordered_map<std::string, TokenType::Type> TokenType::load_token_map(
+    TokenType::Type type,
+    std::string value)
 {
-    return static_cast<int>(type);
+    std::unordered_map<std::string, TokenType::Type> token_map;
+
+    switch (type)
+    {
+    case TokenType::Type::Identifier:
+        token_map.insert({value, type});
+        break;
+    case TokenType::Type::Keyword:
+        token_map.insert({value, type});
+        break;
+    case TokenType::Type::Operator:
+        token_map.insert({value, type});
+        break;
+    case TokenType::Type::Punctuation:
+        token_map.insert({value, type});
+        break;
+    default:
+        break;
+    }
+    return token_map;
 }
